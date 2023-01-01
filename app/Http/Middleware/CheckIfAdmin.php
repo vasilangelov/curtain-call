c0ef08c8c680e24e6a88570d9b4f6cd2437dedbe
vasilangelov <vasilangelov@users.noreply.github.com>
2023-01-01 20:18:45 +0200

    Fix check if user is admin

--- app/Http/Middleware/CheckIfAdmin.php
+++ app/Http/Middleware/CheckIfAdmin.php
@@ -22,19 +22,18 @@ class CheckIfAdmin
      * does not have a '/home' route, use something you've built for your users
      * (again - users, not admins).
      *
-     * @param  \Illuminate\Contracts\Auth\Authenticatable|null  $user
+     * @param \Illuminate\Contracts\Auth\Authenticatable|null $user
      * @return bool
      */
     private function checkIfUserIsAdmin($user)
     {
-        // return ($user->is_admin == 1);
-        return true;
+        return backpack_user()->hasAnyRole('admin', 'superadmin');
     }
 
     /**
      * Answer to unauthorized access request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     private function respondToUnauthorizedRequest($request)
@@ -49,8 +48,8 @@ class CheckIfAdmin
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
@@ -59,7 +58,7 @@ class CheckIfAdmin
             return $this->respondToUnauthorizedRequest($request);
         }
 
-        if (! $this->checkIfUserIsAdmin(backpack_user())) {
+        if (!$this->checkIfUserIsAdmin(backpack_user())) {
             return $this->respondToUnauthorizedRequest($request);
         }
 
